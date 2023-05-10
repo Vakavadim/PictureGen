3404ad6147dbd9aad5ab93fb30882e601b6bcc1f
Vadim Gamzaev <Vadosbox@gmail.com>
2023-05-10 10:00:11 +0300

    рефакторинг search Interactor

--- PictureGen/MainFlow/SearchScene/SearchInteractor.swift
+++ PictureGen/MainFlow/SearchScene/SearchInteractor.swift
@@ -36,19 +36,24 @@ class SearchInteractor: ISearchInteractor {
 	func makeRequest(request: SearchModel.Request) {
 		switch request {
 		case .getImageWithTerm(searchTerm: let search):
-//			DispatchQueue.main.async {
-				self.imageDataFetcher.fetchImageData(from: search.path) { [unowned self] result in
-					switch result {
-					case .success(let data):
-						let pictureData = SearchModel.Response.PictureData(imageData: data, searchTerm: search.term)
-						let response = SearchModel.Response.success(pictureData)
-						self.presenter?.present(response: response)
-					case .failure(let error):
-						let response = SearchModel.Response.failure(error)
-						self.presenter?.present(response: response)
-					}
+			let picture = getPictureFromLocalStorage(searchTerm: search.term)
+			if  picture != nil {
+				guard let imageData = picture?.imageData else {
+					fatalError("no picture data in local storage")
 				}
-//			}
+				guard let requestString = picture?.requestString else {
+					fatalError("no requestString in local storage")
+				}
+				
+				let pictureData = SearchModel.Response.PictureData(
+					imageData: (imageData),
+					searchTerm: (requestString)
+				)
+				let response = SearchModel.Response.success(pictureData)
+				self.presenter?.present(response: response)
+			} else {
+				getPictureFromServer(searchTerm: search.path)
+			}
 		case .savePicture(picture: let picture):
 			if let imageData = picture.image.jpegData(compressionQuality: 1.0) {
 				storageManager.save(picture.searchTerm, imageData: imageData) { [unowned self] _ in
@@ -56,5 +61,29 @@ class SearchInteractor: ISearchInteractor {
 				}
 			}
 		}
+	}
+	
+	func getPictureFromServer(searchTerm: String) {
+		self.imageDataFetcher.fetchImageData(from: searchTerm) { [unowned self] result in
+			switch result {
+			case .success(let data):
+				let pictureData = SearchModel.Response.PictureData(imageData: data, searchTerm: searchTerm)
+				let response = SearchModel.Response.success(pictureData)
+				self.presenter?.present(response: response)
+			case .failure(let error):
+				let response = SearchModel.Response.failure(error)
+				self.presenter?.present(response: response)
+			}
+		}
+	}
+	
+	func getPictureFromLocalStorage(searchTerm: String) -> Picture? {
+		var picture: Picture?
+		storageManager.fetchData { pictures in
+			picture = pictures.first(where: { pic in
+				pic.requestString == searchTerm
+			})
+		}
+		return picture
 	}
 }
